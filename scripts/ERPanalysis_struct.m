ab74660e37bfce8c5708d2d767d82be97cae4ac0
jasonnan2 <59706252+jasonnan2@users.noreply.github.com>
2023-12-29 15:48:41 -0800

    added plotting erp line plots

--- scripts/ERPanalysis_struct.m
+++ scripts/ERPanalysis_struct.m
@@ -17,6 +17,7 @@ classdef ERPanalysis_struct
             
             obj.DATA=myStruct.DATA;
             obj.info=myStruct.info;
+            obj.analysisResults=struct;
             
         end
         
@@ -83,7 +84,7 @@ classdef ERPanalysis_struct
             end
         end
         
-        function obj=plotERP(obj,timeRange,properties)
+        function obj=plotScalpmap(obj,timeRange,properties)
             obj.info.timeRange=timeRange;
             timeNames = fieldnames(timeRange);
             N = numel(fieldnames(obj.DATA));
@@ -109,11 +110,29 @@ classdef ERPanalysis_struct
                         for f=1:length(obj.info.freq_list)
                             figCount = figCount + 1;
                             subplot(3,4,figCount)
-                            s1=squeeze(nanmean(group1(f,:,startIdx:endIdx,:),3)); % get to chan x sub
-                            s2=squeeze(nanmean(group2(f,:,startIdx:endIdx,:),3)); % get to chan x sub
+                            s1=squeeze(nanmean(group1(f,:,startIdx:endIdx,:),3)); % get to chan x sub of first subject
+                            s2=squeeze(nanmean(group2(f,:,startIdx:endIdx,:),3)); % get to chan x sub of second subject
                             
-                            chanlocs=topoSignificance(s1,s2,obj.info.chanlocs,{'.','+'});
-                            plotSigTopo(s1,s2,chanlocs,{'.','+'})
+                            pvals=topoSignificance(s1,s2); % get 1x n channel of p values 
+                            chanlabels={obj.info.chanlocs.labels}; % just the cell array of labels
+                            
+                            chans2add = chanlabels(pvals<0.05); % significant channels to add to list
+                
+                            % Check if the field already exists
+                            if isfield(obj.analysisResults, 'sigElectrodes') && isfield(obj.analysisResults.sigElectrodes, property) && ...
+                                isfield(obj.analysisResults.sigElectrodes.(property), timeNames{t}) && isfield(obj.analysisResults.sigElectrodes.(property).(timeNames{t}), obj.info.freq_list{f})
+                            
+                                % Get the existing cell array
+                                existingArray = obj.analysisResults.sigElectrodes.(property).(timeNames{t}).(obj.info.freq_list{f});
+                                % Check if the new letter is different from the existing ones
+                                chans2add = setdiff(chans2add, existingArray);
+                                obj.analysisResults.sigElectrodes.(property).(timeNames{t}).(obj.info.freq_list{f}) = [existingArray, chans2add];
+                            elseif ~isempty(chans2add)
+                                % Create a new cell array with the new letter
+                                obj.analysisResults.sigElectrodes.(property).(timeNames{t}).(obj.info.freq_list{f}) = chans2add;
+                            end
+
+                            plotSigTopo(s1,s2,obj.info.chanlocs,pvals,{'.','+'})
                             
                             if t==1
                                 title(obj.info.freq_list{f})
@@ -127,9 +146,69 @@ classdef ERPanalysis_struct
                 end
             end
         end
+        
+        function plotERPs(obj,vars2plot,freq2plot,times2plot)
+            N=numel(fieldnames(obj.DATA));
+            timeNames = fieldnames(obj.info.timeRange);
+            sigElectrodes=obj.analysisResults.sigElectrodes;
+            for p=1:length(vars2plot)
+                property=vars2plot{p};
+                time_list = intersect(fieldnames(sigElectrodes.(property)),times2plot);
+
+                for t=1:length(time_list)
+
+                    time=time_list{t};
+                    sig=sigElectrodes.(property).(time);
+                    [~,~,ib] = intersect(fieldnames(sig),freq2plot);
+                    freq_list=freq2plot(sort(ib));
+
+                    % get longest electrode map
+                    maxLength = 0;
+                    for i = 1:length(freq_list)
+                        currentLength = length(sig.(freq_list{i}));  % Get the length of the current cell array
+                        if currentLength > maxLength
+                            maxLength = currentLength;  % Update the maximum length
+                        end
+                    end
+
+                    if ~isempty(freq_list)
+                        figure
+                        for f=1:length(freq_list)
+
+                            for c=1:length(sig.(freq_list{f}))
+
+                                subplot(length(freq_list),maxLength,(f-1)*maxLength+c)
+
+                                freq=freq_list{f};
+                                chan=sig.(freq){c};
+                                freqIdx = find(strcmp(obj.info.freq_list, freq));
+                                timeIdx = obj.info.timeIDX.(time);
+                                elecIdxs = find(strcmp({obj.info.chanlocs.labels},chan ));
+                                data=[];
+                                for n=1:N
+                                    data(:,n) = squeeze(nanmean(obj.DATA.(obj.info.groupNames{n}).(property)(freqIdx,elecIdxs,timeIdx(1):timeIdx(2),:),4));
+                                end
+
+                                plot(obj.info.timeAxis(timeIdx(1):timeIdx(2)),data)
+                                title(chan)
+                                if c==1
+                                    ylabel(freq,'fontweight','bold','fontsize',16)
+                                end
+                            end
+                        end
+                        Lgnd = legend(obj.info.groupNames);
+                        Lgnd.Position(1) = 0.7;
+                        Lgnd.Position(2) = 0.9;
+                        sgtitle(strcat(property,'-',time))
+                    end
+                end
+            end
+
+        end
+        
     end
 end
-
+%%
 function combined = rej5SD(combined)
     disp('5 SD Outlier')
     for f = 1:size(combined, 1)
@@ -141,7 +220,7 @@ function combined = rej5SD(combined)
         end
     end
 end
-
+%%
 function baselineCorrected = baselineCorrection(cleaned,baselineIDX)
     disp('Baseline Correction')
     baselineCorrected=nan(size(cleaned));
@@ -157,7 +236,13 @@ function baselineCorrected = baselineCorrection(cleaned,baselineIDX)
 end
 
 %% Plotting scalpmap with significant electrodes
-function new_chan = plotSigTopo(s1,s2,chanlocs,key)
+function new_chan = plotSigTopo(s1,s2,chanlocs,pvals, key)
+
+    %[~,p]=fdr(reshape(p,[1,numel(p)]),0.05);
+    mask=key((pvals<0.05)+1);
+    [chanlocs.labels]=mask{:};
+    
+    
     % s1 and s2 are chan x sub size
     plotdata=nanmean(s2,2)-nanmean(s1,2);
     topoplot(plotdata,chanlocs,'headrad','rim','electrodes','labels','efontsize' ,16); 
@@ -167,32 +252,29 @@ function new_chan = plotSigTopo(s1,s2,chanlocs,key)
     brighten(0.5)
 end
 
-%% Function to calculate which electrodes are significant btw two groups
-function chanlocs=topoSignificance(s1,s2,chanlocs,key)
 
-    % pre and post are data matrices size N subjects x C channels
+
+%% Function to calculate which electrodes are significant btw two groups
+function pvals=topoSignificance(s1,s2)
+
+    % s1 and s2 are data matrices size C channels x N subjects
     % key is a two element cell array with markers for non-sig and sig
     % new_chan is the chan_locs
     % Perform t-tests
     
     s1=squeeze(s1);
     s2=squeeze(s2);
-    for chan = 1:length(chanlocs)
+    for chan = 1:size(s1,1)
         % Get non-NaN indices
-        [~,p(chan)] = ttest2(s1(chan,:), s2(chan,:));
-        if isnan(p(chan))
+        [~,pvals(chan)] = ttest2(s1(chan,:), s2(chan,:));
+        if isnan(pvals(chan))
             disp('asdfawrsasdbaweasbas')
             waitforbuttonpress
         end
-        
     end
-    %[~,p]=fdr(reshape(p,[1,numel(p)]),0.05);
-    mask=key((p<0.05)+1);
-    [chanlocs.labels]=mask{:};
+
 end
         
-
-
 
 
 
--- scripts/ERPwithStruct.m
+++ scripts/ERPwithStruct.m
@@ -26,106 +26,14 @@ analysis = analysis.standardPipeline(baselineTime); % standard processing pipeli
 timeRange.choice=[0 500];
 timeRange.imReward=[500 1000];
 timeRange.cumReward=[1000 1500];
-Vars2Plot={'EVgain'};
-analysis = analysis.plotERP(timeRange,Vars2Plot);
-%%
+vars2plot={'EVgain'};
+analysis = analysis.plotScalpmap(timeRange,vars2plot);
 
+vars2plot={'EVgain'};
+freq2plot={'theta','alpha','beta'};
+times2plot={'choice','imReward','cumReward'};
 
-topoplot(zeros(1,24),chanlocs_str,'headrad','rim','electrodes','labels'); 
-%%
+analysis.plotERPs(vars2plot,freq2plot,times2plot)
 
-% 'EVgain-alpha-imReward-CPz-C3-F7'
-% 'EVgain-alpha-cumReward-CPz'
-% 'EVgain-alpha-choice-F7-P7-O1'
-% 
-% 'EVgain-beta-imReward-C3-FC3-FP2'
-% 'EVgain-beta-cumReward-C3-FC3-FP2'
-% 'EVgain-beta-choice-C3'
-% 
-% 'EVgain-theta-imreward-P3'
-% 'EVgain-theta-choice-FC3'
-
-EVgain_alpha = struct('freq', 'alpha', 'metric', 'EVgain', 'times', {'imReward', 'cumReward', 'choice'}, 'electrodes', {{'CPz', 'C3', 'F7'}, {'CPz'}, {'F7', 'P7', 'O1'}});
-EVgain_beta = struct('freq', 'beta', 'metric', 'EVgain', 'times', {'imReward', 'cumReward', 'choice'}, 'electrodes', {{'C3', 'FC3', 'FP2'}, {'C3', 'FC3', 'FP2'}, {'C3'}});
-EVgain_theta = struct('freq', 'theta', 'metric', 'EVgain', 'times', {'imReward', 'choice'}, 'electrodes', {{'P3'}, {'FC3'}});
-
-%% Plotting Line plots for EVgain
-
-obj=analysis;
-p=1;
-N=numel(fieldnames(obj.DATA));
-property=Vars2Plot{p};
-timeNames = fieldnames(timeRange);
-N = numel(fieldnames(obj.DATA));
-combinations = nchoosek(1:N,2); % This will give you a matrix where each row is a combination of two groups
-
-for p=1:length(properties)
-    property=properties{p};
     
-    for n=1:N
-        data(:,:,:,n) = nanmean(obj.DATA.(obj.info.groupNames{n}).(property),4);
-    end
-
-    figure
-    figCount=0;
-    for t=1:length(timeNames)
-        for f=1:length(obj.info.freq_list)
-            figCount = figCount + 1;
-            subplot(3,4,figCount)
-
-            [~, startIdx] = min(abs(obj.info.timeAxis - timeRange.(timeNames{t})(1)));
-            [~, endIdx] = min(abs(obj.info.timeAxis - timeRange.(timeNames{t})(2)));
-
-            s1=squeeze(nanmean(group1(f,:,startIdx:endIdx,:),3)); % get to chan x sub
-            s2=squeeze(nanmean(group2(f,:,startIdx:endIdx,:),3)); % get to chan x sub
-            plotSigTopo(s1,s2,obj.info.chanlocs,{'.','+'})
-            
-            
-        end
-    end
-end
-
-            
-            
-%%
-varSets = {
-'EVgain-alpha-imReward-CPz-C3-F7',
-'EVgain-alpha-cumReward-CPz',
-'EVgain-alpha-choice-F7-P7-O1',
-'EVgain-beta-imReward-C3-FC3-FP2',
-'EVgain-beta-cumReward-C3-FC3-FP2',
-'EVgain-beta-choice-C3',
-'EVgain-theta-imreward-P3',
-'EVgain-theta-choice-FC3'}
-
-varString=varSets{1}
-[metric,freqIdx,timeIdx,elecIdxs]=splitConditions(obj,varString)
-%%
-function [metric,freqIdx,timeIdx,elecIdxs]=splitConditions(obj,varString)
-    % Loop over each variable set
-    % Split the variable set into its components
-    components = split(varString, '-');
-    metric = components{1};
-    freq = components{2};
-    time = components{3};
-    elecs = components(4:end);
-
-    % Find the indices of the components in the dataset
-    freqIdx = find(strcmp(obj.info.freq_list, freq));
-    timeIdx = obj.info.timeIDX.(time);
-    elecIdxs = find(ismember({obj.info.chanlocs.labels}, elecs));
-end  
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
             
