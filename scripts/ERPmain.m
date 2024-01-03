29f01836c5cb7b4cf687971fd3041f7e194549f1
jasonnan2 <59706252+jasonnan2@users.noreply.github.com>
2024-01-03 11:39:42 -0800

    Added brain network plots in the class

--- scripts/ERPmain.m
+++ scripts/ERPmain.m
@@ -6,34 +6,70 @@ cd('C:\Users\jason\OneDrive\Desktop\MS Beng\Neatlabs\Climate_LuckyDoor')
 addpath('scripts')
 addpath('A:\eeglab_OldLSL_DataAna04072023')
 eeglab
-%% Main Analysis with Class
-% Create an object of the class
+%% Loading in data and general preprocessing
+% loading in data
 dataPath='A:/ClimateLD/analysis_results/ClimateLD_allgroups_amp_rmv.mat';
 load(dataPath)
-variables = {'EVgain', 'EVloss', 'GLbias'};
+% Defining time in mS for baseline correction
 baselineTime=[-250 -50];
-
-analysis = ERPanalysis(CLIMATELD); % set up the class, need filepath of group data, variables to run and baseline correction range
-analysis = analysis.cleanDatasets(); % remove any missing subjects
-analysis = analysis.standardPipeline(baselineTime); % standard processing pipeline, 5SD outlier, baseline correction
-
-%%
-
-% choice 0-500
-% immediate reward 500-1000
-% cum reward 1000-1500
-
+% set up the class for Scalp analysis, needs the data structure
+analysis = ERPanalysis(CLIMATELD);
+% remove any missing subjects if any from Scalp data
+analysis = analysis.cleanDatasets('scalpData'); 
+% standard processing pipeline, 5SD outlier, baseline correction
+analysis = analysis.standardPipeline(baselineTime,'scalpData'); 
+% repeat for sourceData
+analysis = analysis.cleanDatasets('sourceData'); 
+analysis = analysis.standardPipeline(baselineTime,'sourceData'); 
+%% Plotting scalp maps and ERP
+% Defining time ranges of interest
 timeRange.choice=[0 500];
 timeRange.imReward=[500 1000];
 timeRange.cumReward=[1000 1500];
+% Plotting scalp topo plots for variables in vars2plot and each timeRange
+% speficied
 vars2plot={'EVgain'};
 analysis = analysis.plotScalpmap(timeRange,vars2plot);
-
+% Plotting significant channels from topo plots as line plots with all
+% groups overlayed. Only selecting conditions which are in the specified
+% vectors for var, frequency, and time ranges. 
 vars2plot={'EVgain'};
-freq2plot={'theta','alpha','beta'};
+freq2plot={'broadband'};
 times2plot={'choice','imReward','cumReward'};
-
 analysis.plotERPs(vars2plot,freq2plot,times2plot)
 
-    
-            
+% plot network
+analysis.plotNetwork(netwrk,timeRange,vars2plot)
+%% Plotting Source Activity
+
+fpn=[5 6 55 66 59 60]; con=[3 4 19 20 37 38 39 40 41 42 57 58 67 68];
+admn=[11 12 25 26 29 30 53 54];
+pdmn=[15 16 21 22 51 52];
+mtldmn=[9 10 17 18 31 32 35 36 61 62 65 66 1];
+vis=[7 8 13 14 23 24 27 28 43 44];
+sm=[33 34 49 50 45 46];
+van=[2 47 48 63 64];
+
+netwrk(1).name='FPN';
+netwrk(1).roi=fpn;
+netwrk(2).name='CON';
+netwrk(2).roi=con;
+netwrk(3).name='aDMN';
+netwrk(3).roi=admn;
+netwrk(4).name='pDMN';
+netwrk(4).roi=pdmn;
+netwrk(5).name='mtlDMN';
+netwrk(5).roi=mtldmn;
+netwrk(6).name='Visual';
+netwrk(6).roi=vis;
+netwrk(7).name='SM';
+netwrk(7).roi=sm;
+netwrk(8).name='VAN';
+netwrk(8).roi=van;
+
+
+
+%%
+
+
+
--- scripts/formattingData.m
+++ scripts/formattingData.m
@@ -1,34 +1,62 @@
 CLIMATELD=struct();
-load('A:\ClimateLD\analysis_results\climateLDcontrol.mat')
-CLIMATELD.DATA.Control.groupname='Control';
-CLIMATELD.DATA.Control.EVgain=EVgain;
-CLIMATELD.DATA.Control.EVloss=EVloss;
-CLIMATELD.DATA.Control.GLbias=GLbias;
-CLIMATELD.DATA.Control.subList=subjectcoll;
+% Scalp Data
+load('A:\ClimateLD\analysis_results\climateLDcontrol_amp_rmv.mat')
+CLIMATELD.scalpData.Control.groupname='Control';
+CLIMATELD.scalpData.Control.EVgain=EVgain;
+CLIMATELD.scalpData.Control.EVloss=EVloss;
+CLIMATELD.scalpData.Control.GLbias=GLbias;
+CLIMATELD.scalpData.Control.subList=subjectcoll;
 
-load('A:\ClimateLD\analysis_results\climateLDwitnessed.mat')
-CLIMATELD.DATA.Witnessed.groupname='Witnessed';
-CLIMATELD.DATA.Witnessed.EVgain=EVgain;
-CLIMATELD.DATA.Witnessed.EVloss=EVloss;
-CLIMATELD.DATA.Witnessed.GLbias=GLbias;
-CLIMATELD.DATA.Witnessed.subList=subjectcoll;
+load('A:\ClimateLD\analysis_results\climateLDwitnessed_amp_rmv.mat')
+CLIMATELD.scalpData.Witnessed.groupname='Witnessed';
+CLIMATELD.scalpData.Witnessed.EVgain=EVgain;
+CLIMATELD.scalpData.Witnessed.EVloss=EVloss;
+CLIMATELD.scalpData.Witnessed.GLbias=GLbias;
+CLIMATELD.scalpData.Witnessed.subList=subjectcoll;
 
-load('A:\ClimateLD\analysis_results\climateLDhappenedto.mat')
-CLIMATELD.DATA.HappenedTo.groupname='HappenedTo';
-CLIMATELD.DATA.HappenedTo.EVgain=EVgain;
-CLIMATELD.DATA.HappenedTo.EVloss=EVloss;
-CLIMATELD.DATA.HappenedTo.GLbias=GLbias;
-CLIMATELD.DATA.HappenedTo.subList=subjectcoll;
+load('A:\ClimateLD\analysis_results\climateLDhappenedto_amp_rmv.mat')
+CLIMATELD.scalpData.HappenedTo.groupname='HappenedTo';
+CLIMATELD.scalpData.HappenedTo.EVgain=EVgain;
+CLIMATELD.scalpData.HappenedTo.EVloss=EVloss;
+CLIMATELD.scalpData.HappenedTo.GLbias=GLbias;
+CLIMATELD.scalpData.HappenedTo.subList=subjectcoll;
+
+% source Data
+load('A:\ClimateLD\analysis_results\climateLDcontrol_source_amp_rmv.mat')
+CLIMATELD.sourceData.Control.groupname='Control';
+CLIMATELD.sourceData.Control.EVgain=EVgain;
+CLIMATELD.sourceData.Control.EVloss=EVloss;
+CLIMATELD.sourceData.Control.GLbias=GLbias;
+CLIMATELD.sourceData.Control.subList=subjectcoll;
+
+load('A:\ClimateLD\analysis_results\climateLDwitnessed_source_amp_rmv.mat')
+CLIMATELD.sourceData.Witnessed.groupname='Witnessed';
+CLIMATELD.sourceData.Witnessed.EVgain=EVgain;
+CLIMATELD.sourceData.Witnessed.EVloss=EVloss;
+CLIMATELD.sourceData.Witnessed.GLbias=GLbias;
+CLIMATELD.sourceData.Witnessed.subList=subjectcoll;
+
+load('A:\ClimateLD\analysis_results\climateLDhappenedto_source_amp_rmv.mat')
+CLIMATELD.sourceData.HappenedTo.groupname='HappenedTo';
+CLIMATELD.sourceData.HappenedTo.EVgain=EVgain;
+CLIMATELD.sourceData.HappenedTo.EVloss=EVloss;
+CLIMATELD.sourceData.HappenedTo.GLbias=GLbias;
+CLIMATELD.sourceData.HappenedTo.subList=subjectcoll;
 
 CLIMATELD.info.freq_list=freq_list;
 CLIMATELD.info.timeAxis=timeAxis;
-CLIMATELD.info.groupNames=fieldnames(CLIMATELD.DATA);
+CLIMATELD.info.groupNames=fieldnames(CLIMATELD.scalpData);
 
-CLIMATELD.info.variables=setdiff(fieldnames(CLIMATELD.DATA.Control),{'groupname','subList'});
-
+CLIMATELD.info.variables=setdiff(fieldnames(CLIMATELD.scalpData.Control),{'groupname','subList'});
 
 load('chanlocs_file.mat') % chanlocs_str
 CLIMATELD.info.chanlocs=chanlocs_str;
-save('A:\ClimateLD\analysis_results\ClimateLD_allgroups.mat','CLIMATELD')
+
+load('68roi.mat')
+CLIMATELD.info.roi=roi;
+
+
+save('A:\ClimateLD\analysis_results\ClimateLD_allgroups_amp_rmv.mat','CLIMATELD')
+
 
 
