7af8c09b64a783d0b466497d29710474049130de
jasonnan2 <59706252+jasonnan2@users.noreply.github.com>
2024-01-31 17:17:54 -0800

    Fixed bug with subject list as column vector instead of row

--- scripts/ERSPmain.m
+++ scripts/ERSPmain.m
@@ -5,7 +5,7 @@ addpath('scripts/DataAnalysis')
 addpath('scripts/DataAnalysis/functions')
 load('C:\Users\jason\OneDrive\Desktop\MS Beng\Neatlabs\Climate_LuckyDoor\scripts\New Folder\subjectcoll.mat')
 %% Loading in data and definitions
-dataPath='ERSP_sample.mat'
+dataPath='A:/ClimateLD/analysis_results/formattedData/ERSP_sample.mat'
 load(dataPath)
 
 %% ERSP Analysis
@@ -20,26 +20,27 @@ freqRanges.alpha=[8 13];
 freqRanges.beta=[13,30];
 
 erspObject=ERSPObject(ESRP.DATA, ESRP.info, baselineTime, timeRange); % Initialize scalp processing
-scalpObj = erspObject.createERP(freqRanges); % convert ersp data to ERP data by averaging freq. 
 erspObject = erspObject.cleanDatasets(); % remove any missing subjects if any from Scalp data
 erspObject = erspObject.standardProcessing('outlier','3MAD'); % standard processing pipeline, 5SD outlier, baseline correction
+scalpObj = erspObject.createERP(freqRanges); % convert ersp data to ERP data by averaging freq. 
 
 erspObject.plotERSP('chans2plot',{'Oz'},'times2plot',{'short'})
 
 %%
-timeRange.theta=[244 577];
-timeRange.alpha=[211 550];
-timeRange.beta=[314 592];
+timeRange=struct();
+timeRange.t=[244 577];
+timeRange.a=[211 550];
+timeRange.b=[314 592];
 
 scalpObject=ScalpObject(scalpObj.DATA, scalpObj.info, baselineTime, timeRange); % Initialize scalp processing
-scalpObject = scalpObject.standardProcessing('outlier','3MAD','calBaseline',0); % standard processing pipeline, 5SD outlier, baseline correction
+scalpObject = scalpObject.standardProcessing('outlier','none','calBaseline',0); % standard processing pipeline, 5SD outlier, baseline correction
 scalpObject = scalpObject.ScalpAnalysis(); % calcualtes all significant electrodes between groups across all conditions
-%scalpObject = scalpObject.calSigTbl(); % creating table of significant neural attributes
+scalpObject = scalpObject.calSigTbl(); % creating table of significant neural attributes
 
 % Scalp Plotting
 close all
-vars2plot={'emotion'}; freq2plot={'alpha'}; times2plot={'choice'}; errorType='sem';
-scalpObject.plotScalpMap('vars2plot',vars2plot,'freq2plot',{'alpha'},'times2plot',{'alpha'});
+vars2plot={'emotion'}; errorType='sem';
+scalpObject.plotScalpMap('vars2plot',vars2plot,'freq2plot',{'alpha'},'times2plot',{'a'});
 caxis([-5 5])
 %scalpObject.plotScalpMap('vars2plot',vars2plot,'freq2plot',{'theta'},'times2plot',{'theta'});
 %scalpObject.plotScalpMap('vars2plot',vars2plot,'freq2plot',{'beta'},'times2plot',{'beta'});
